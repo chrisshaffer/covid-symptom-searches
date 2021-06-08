93e07b7d05edb2568f11b58cd7f22c465abc4e89
Chris Shaffer <christophermshaffer@gmail.com>
2021-06-08 16:29:28 -0700

    preliminary eda

---
+++ notebooks/clean_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def clean_data(searches,hospitalizations):
+        
+    df = pd.merge(searches,hospitalizations, how='outer', on = ['key','date'])
+    df.fillna(0,inplace=True)
+    
+    return df
