f26117c8f98ce584545c393de3cd7256401bea3c
Chris Shaffer <christophermshaffer@gmail.com>
2021-06-10 11:21:50 -0700

    start of hyp testing

--- notebooks/clean_data.py
+++ notebooks/clean_data.py
@@ -3,6 +3,6 @@ import pandas as pd
 def clean_data(searches,hospitalizations):
         
     df = pd.merge(searches,hospitalizations, how='outer', on = ['key','date'])
-    df.fillna(0,inplace=True)
+    # df.fillna(0,inplace=True)
     
     return df
